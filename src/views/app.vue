4864b7127de7f0bd36ec71ea00574a493825610b
huanghaojie <1107408003@qq.com>
2019-05-17 17:16:45 +0800

    我的待办初步搭建 1

---
+++ src/views/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div style="flex:1">
+        <myPending></myPending>
+    </div>
+</template>
+
+<!--引入bui-weex样式文件-->
+<style lang="scss" src="bui-weex/src/css/buiweex.scss"></style>
+
+<script>
+    import myPending from "./myPending.vue";
+    module.exports = {
+        data () {
+            return {}
+        },
+        components: {
+          myPending
+        }
+    }
+</script>
---
+++ src/views/myPending.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="main">
+    <!-- 热门话题 -->
+    <div class="my-pending bra">
+      <div class="my-pending-title flex">
+        <text class="f28 fw5 c0">我的待办</text>
+        <text class="f24 c153 fw4 pl20 pt10 pb10" @click="myPengdingMoreEvent">全部</text>
+      </div>
+      <div class="my-pending-content flex">
+        <div v-for="(item, index) in myPengdingArr" :key='index' @click='myPengdingUserEvent'>
+          <text class="c0 f36 tc">10</text>
+          <text class="f26 c102 fw4 mt10">张珊珊</text>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        myPengdingArr:['','','','']
+      }
+    },
+    methods: {
+      // 更多
+      myPengdingMoreEvent() {
+        this.$alert()
+      },
+      // 常用联系人
+      myPengdingUserEvent(){
+        this.$alert()
+      }
+    }
+  }
+</script>
+
+<style lang="css" src="../css/common.css"></style>
+<style>
+  .main {
+    flex: 1;
+    padding-top: 100px;
+    background-color: #666;
+  }
+
+  .my-pending {
+    background-color: #fff;
+  }
+
+  .my-pending-title {
+    height: 40px;
+    margin: 18px 23px 20px 25px;
+  }
+
+  .my-pending-content {
+    margin: 13px 62px 32px 62px;
+  }
+</style>
