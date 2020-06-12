0b74f8f58054ace267988d58a31cae75b6162e59
黄好杰 <13612989029@163.com>
2020-06-12 09:29:43 +0800

    保存一下

--- src/views/myPending.vue
+++ src/views/myPending.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="main">
+  <div ref="wrap">
     <!-- 热门话题 -->
-    <div class="my-pending bra">
+    <div class="my-pending">
       <div class="my-pending-title flex">
         <text class="f28 fw5 c0">我的待办</text>
         <text class="f24 c153 fw4 pl20 pt10 pb10" @click="myPengdingMoreEvent">全部</text>
       </div>
       <div class="my-pending-content flex">
         <div v-for="(item, index) in myPengdingArr" :key='index' @click='myPengdingUserEvent'>
-          <text class="c0 f36 tc">10</text>
-          <text class="f26 c102 fw4 mt10">张珊珊</text>
+          <text class="c0 f36 tc">{{index+8}}</text>
+          <text class="f26 c102 fw4 mt10">{{item}}</text>
         </div>
       </div>
     </div>
@@ -20,7 +20,7 @@
   export default {
     data() {
       return {
-        myPengdingArr:['','','','']
+        myPengdingArr: ['日程', '待办', '项目', '邮件']
       }
     },
     methods: {
@@ -29,20 +29,32 @@
         this.$alert()
       },
       // 常用联系人
-      myPengdingUserEvent(){
+      myPengdingUserEvent() {
         this.$alert()
+      },
+      broadcastWidgetHeight() {
+        let _params = this.$getPageParams();
+        setTimeout(() => {
+          dom.getComponentRect(this.$refs.wrap, (ret) => {
+            var channel = new BroadcastChannel('WidgetsMessage')
+            channel.postMessage({
+              widgetHeight: ret.size.height,
+              id: _params.id
+            });
+            channel.close();
+          });
+        }, 100)
       }
+    },
+    mounted() {
+      this.broadcastWidgetHeight()
     }
   }
 </script>
 
 <style lang="css" src="../css/common.css"></style>
 <style>
-  .main {
-    flex: 1;
-    padding-top: 100px;
-    background-color: #666;
-  }
+  .main {}
 
   .my-pending {
     background-color: #fff;
